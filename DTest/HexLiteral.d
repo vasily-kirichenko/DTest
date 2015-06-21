16559ffd494720f1e6cb1128649cde9ccac90f9c
Vasily Kirichenko <vaskir@gmail.com>
2015-06-21 22:03:48 +0300

    try to implement hex literals

---
+++ DTest/HexLiteral.d
@@ -0,0 +1,17 @@
+﻿module HexLiteral;
+
+import std.conv;
+import std.stdio;
+
+auto hexImpl(byte length, string s) {
+	return cast(immutable(ubyte)[])s;
+}
+
+template sha1(string s) {
+	enum hex = hexImpl(20, s);
+}
+
+unittest {
+	//writeln(sha1!"ffff");
+	writeln(to!string(typeof(cast(immutable(ubyte)[])x"ff")));
+}
