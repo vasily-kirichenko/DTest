52f5dabd7c1b8d3b458e013803102e4839fe481a
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2015-06-22 16:37:22 +0300

    crufting HexLiteral template

--- DTest/HexLiteral.d
+++ DTest/HexLiteral.d
@@ -2,16 +2,35 @@
 
 import std.conv;
 import std.stdio;
+import std.conv;
+import std.array;
+import std.range;
+import std.algorithm;
+import std.string;
 
-auto hexImpl(byte length, string s) {
-	return cast(immutable(ubyte)[])s;
-}
+//struct HexChar {
+//	private static immutable bool[char] validChars;
+//	static this() {
+//		foreach(c; iota(0, 16, 1).map!(x => to!char(format("%01X", x))))
+//			validChars[c] = true;
+//	}
+//	static bool isHex(char c) { 
+//		if (c in validChars) return true;
+//		else return false;
+//	}
+//}
 
-template sha1(string s) {
-	enum hex = hexImpl(20, s);
+template sha1(string hexStr) {
+	static if (hexStr.length != 40)
+		static assert (0, "Hex string should be of length 40, but it's length is " 
+						  ~ to!string(hexStr.length) ~ ".");
+	enum sha1 = 
+		hexStr
+		.chunks(2)
+		.map!(twoDigits => twoDigits.parse!ubyte(16))
+		.array();
 }
 
 unittest {
-	//writeln(sha1!"ffff");
-	writeln(to!string(typeof(cast(immutable(ubyte)[])x"ff")));
+	auto x = sha1!"0102030405060708090A0B0C0D0E0F101213141g";
 }
