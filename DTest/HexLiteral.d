6c94b110df5f45ac3a8d0bfce676fb656558349a
Vasily Kirichenko <vaskir@gmail.com>
2015-07-02 21:05:29 +0300

    refactor HexLiteral

--- DTest/HexLiteral.d
+++ DTest/HexLiteral.d
@@ -5,25 +5,25 @@ import std.stdio;
 import std.range;
 import std.algorithm;
 import std.string;
+import std.format;
 
-struct HexString {
-	private static auto getValidChars() {
+template hexString(const string str) {
+	auto pure @property validChars() {
 		bool[dchar] res;
 		foreach(c; iota(0, 16, 1).map!(x => to!dchar(format("%01X", x))))
 			res[c] = true;
 		return res;
-	}
-	static bool isValid(const string str) {
-		auto validChars = getValidChars();
+	};
+
+	auto pure hexString() {
 		return str.filter!(x => !(x in validChars)).count == 0;
-	}
+	};
 }
 
 template sha1(string hexStr) {
 	static if (hexStr.length != 40)
-		static assert (0, "Hex string should be of length 40, but it's length is " 
-						  ~ to!string(hexStr.length) ~ ".");
-	static if (!HexString.isValid(hexStr))
+		static assert (0, format("Hex string should be of length 40, but it's length is %d.", hexStr.length));
+	static if (!hexString!hexStr)
 		static assert (0, "Invalid char in hex string.");
 	enum sha1 = 
 		hexStr
